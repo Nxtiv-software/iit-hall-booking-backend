649eeb90a70ac3d98a1181f43f89beca7545c449
Lashan Gunawardena <ravijalashangunawardena@gmail.com>
2025-11-23 02:28:12 +0530

    feat: implement admin and student profile management APIs; make user fields optional in schema

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
-import todoRoutes from "./routes/todoRoutes.js";
+// import todoRoutes from "./routes/todoRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import bookingRoutes from "./routes/bookings.js";
-import approveRoutes from "./routes/approvals.js";
+import userRoutes from "./routes/userRoutes.js";
+import studentRoutes from "./routes/studentRoutes.js"
+// import bookingRoutes from "./routes/bookings.js";
+// import approveRoutes from "./routes/approvals.js";
 import authMiddleware from "./middleware/authMiddleware.js";
 import adminMiddleware from "./middleware/adminMiddleware.js";
 
@@ -24,14 +26,15 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log("HI");
-  res.send(200);
+  res.json("Welcome to IIT Hall Booking");
 });
 
 app.use("/auth", authRoutes);
-app.use("/admin", authMiddleware, adminMiddleware, adminRoutes);
-app.use("/todos", authMiddleware, todoRoutes);
-app.use("/bookings", authMiddleware, bookingRoutes);
-app.use("/approvals", authMiddleware, approveRoutes);
+app.use("/users", authMiddleware, userRoutes);
+app.use("/students", authMiddleware, studentRoutes);
+app.use("/admins", authMiddleware, adminMiddleware, adminRoutes);
+// app.use("/todos", authMiddleware, todoRoutes);
+// app.use("/bookings", authMiddleware, bookingRoutes);
+// app.use("/approvals", authMiddleware, approveRoutes);
 
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
